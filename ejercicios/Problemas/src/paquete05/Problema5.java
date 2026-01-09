62efc1ff4253d41319f39c74775d5bb6c2f95511
Adrian Lopez <israelruiz2874@gmail.com>
2026-01-09 09:13:36 -0500

    Problema5

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,7 +10,37 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
-    int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
-    
+
+    public static void main(String[] args) {
+
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+
+        int[][] resultado = sumaDeArreglos(informacion, informacion2);
+
+        for (int i = 0;
+                i < resultado.length;
+                i++) {
+            for (int j = 0; j < resultado[i].length; j++) {
+                System.out.print(resultado[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] sumaDeArreglos(int[][] a, int[][] b) {
+
+        int filas = a.length;
+        int columnas = a[0].length;
+
+        int[][] c = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        return c;
+    }
 }
